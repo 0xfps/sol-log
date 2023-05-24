13c036ac09990aab9b11707f6441e4250fb76313
fps <anthony.nnaemeka.umeh@gmail.com>
2023-05-24 10:46:12 +0100

    📝 + Comments.

--- src/Log.sol
+++ src/Log.sol
@@ -2,14 +2,20 @@
 pragma solidity 0.8.19;
 
 /**
-* @title Logarithm
+* @title Log (Logarithm)
 * @author fps (@0xfps).
 * @dev A simple solidity logarithm library.
+* @notice For logarithms, refer to http://rb.gy/xvkke.
 */
 
 library Log {
-    /// @dev    Returns the value of log(b)
-    ///                                 a
+    /**
+    * @dev    Returns the value of log(b)
+    *                                 a
+    * @param a Base.
+    * @param b Number.
+    * @return p Power.
+    */
     function log(uint256 a, uint256 b) internal pure returns (uint256 p) {
         while (b > a) {
             b /= a;
