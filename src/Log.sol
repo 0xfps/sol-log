7f40b9126c96587247cd4c9f2b31c64c6fc3c0ef
fps <anthony.nnaemeka.umeh@gmail.com>
2023-05-24 11:22:55 +0100

    🐛 Improvements.

--- src/Log.sol
+++ src/Log.sol
@@ -16,10 +16,24 @@ library Log {
     * @param b Base.
     * @return p Power.
     */
-    function log(uint256 x, uint256 b) internal pure returns (uint256 p) {
-        while (x > b) {
+    function _log(uint256 x, uint256 b) private pure returns (uint256 p) {
+        if (b <= 1) revert("LOG: INVALID_BASE");
+        
+        while (x >= b) {
             x /= b;
             ++p;
         }
+    }
+
+    function log(uint256 x, uint256 b) internal pure returns (uint256) {
+        return _log(x, b);
+    }
+
+    function logB10(uint256 x) internal pure returns (uint256) {
+        return log(x, 10);
+    }
+
+    function logMul(uint256 x, uint256 y, uint256 b) internal pure returns (uint256) {
+        return log(x, b) + log(y, b);
     }
 }
