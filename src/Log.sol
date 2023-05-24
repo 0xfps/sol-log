c114a1d056ed49e91db70094d3fc6191a036b4ab
fps <anthony.nnaemeka.umeh@gmail.com>
2023-05-24 11:48:55 +0100

    🐛 Improvements.

--- src/Log.sol
+++ src/Log.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.19;
 /**
 * @title Log (Logarithm)
 * @author fps (@0xfps).
-* @dev A simple solidity logarithm library.
+* @dev A simple solidity positive-only logarithm library.
 * @notice For logarithms, refer to http://rb.gy/xvkke.
 */
 
@@ -17,8 +17,8 @@ library Log {
     * @return p Power.
     */
     function _log(uint256 x, uint256 b) private pure returns (uint256 p) {
-        if (b <= 1) revert("LOG: INVALID_BASE");
-        
+        if (b < 2) revert("LOG: INVALID_BASE");
+
         while (x >= b) {
             x /= b;
             ++p;
@@ -35,5 +35,18 @@ library Log {
 
     function logMul(uint256 x, uint256 y, uint256 b) internal pure returns (uint256) {
         return log(x, b) + log(y, b);
+    }
+
+    function logDiv(uint256 x, uint256 y, uint256 b) internal pure returns (uint256) {
+        return log(x, b) - log(y, b);
+    }
+
+    function logPow(uint256 x, uint256 p, uint256 b) internal pure returns (uint256) {
+        return p * log(x, b);
+    }
+
+    function logRt(uint256 x, uint256 r, uint256 b) internal pure returns (uint256) {
+        if (r == 0) revert("LOG: ZERO_DIVISION");
+        return log(x, b) / r;
     }
 }
